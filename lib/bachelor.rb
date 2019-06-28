f31f01303d065c40cd5b75a581b84071e6fa3cb5
brinda behal <brindab50@gmail.com>
2019-06-28 17:42:20 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,10 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |contestant|
+    if contestant["status"].downcase == "winner" 
+    return contestant[:name].split(" ").first
+      
+    end
+    end
 end
 
 def get_contestant_name(data, occupation)
