05970b2a2f1528b476cda6ece06ebf391c189691
brinda behal <brindab50@gmail.com>
2019-07-01 01:55:09 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -18,9 +18,18 @@ def get_contestant_name(data, occupation)
 end
 end
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  #binding.pry
+  contestants_number = 0
+  data.each do |season, contestants|
+     contestants.each do |contestant|
+    if contestant["hometown"] == hometown
+     contestants_number += 1
+    end 
+   end 
+ 
+  end
+ return contestants_number
 end
-
 def get_occupation(data, hometown)
   # code here
 end
